9ebac9701c7b778242bb4c236b5afae4ea11f59f
AllisterF1 <allytheforbes@gmail.com>
2023-05-15 10:18:19 +0100

    undefined token in user fixed

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ function App() {
               : (page === 'u')
                 ? <Users jwtToken={jwt} setPage={setPage} setSelectedUser={setSelectedUser} user={user} />
                 : (page === 'v')
-                  ? <User setPage={setPage} selectedUser={selectedUser} user={user} />
+                  ? <User  jwtToken={jwt} setPage={setPage} selectedUser={selectedUser} user={user} />
                   : <Events setPage={setPage} user={user} setEvent={setEvent} setCities={setCities} dma={607} />
           : (page === 'r')
             ?
